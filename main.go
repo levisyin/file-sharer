e47022d2c4301654b581b7f3ed78ea09809173f0
levisyin <lilassherl@gmail.com>
2024-05-03 22:34:27 +0800

    mvp

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ import (
 	"os"
 )
 
+type FileInfo struct {
+	Name string
+}
+
 var (
 	//go:embed index.html
 	homePage embed.FS
@@ -17,24 +21,42 @@ var (
 
 func main() {
 	http.HandleFunc("/", home)
-	http.FileServer(http.FS(homePage))
 	http.HandleFunc("/uploadFile", uploadFile)
 	http.ListenAndServe(":8080", nil)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	tpl, err := template.ParseFS(homePage)
+	tpl, err := template.ParseFS(homePage, "index.html")
 	if err != nil {
 		slog.With("err", err, "uri", r.RequestURI).Error("index.html not found in pages cache")
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
-	data := map[string]interface{}{
-		"userAgent": r.UserAgent(),
+
+	dir, err := os.Getwd()
+	if err != nil {
+		slog.With("err", err, "uri", r.RequestURI).Error("Getwd error")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		slog.With("err", err, "uri", r.RequestURI).Error("ReadDir error")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var fileInfos []FileInfo
+	for _, file := range files {
+		fileInfos = append(fileInfos, FileInfo{Name: file.Name()})
+	}
+	data := struct{ Files []FileInfo }{fileInfos}
 	if err := tpl.Execute(w, data); err != nil {
+		slog.With("err", err, "uri", r.RequestURI).Error("Execute tpl error")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
@@ -43,24 +65,32 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 	// upload size
 	err := r.ParseMultipartForm(1024000) // grab the multipart form
 	if err != nil {
-		fmt.Fprintln(w, err)
+		slog.With("err", err).Error("ParseMultipartForm error")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	//reading original file
-	file, handler, err := r.FormFile("originalFile")
-	if err != nil {
-		slog.With("err", err).Error("retrieving file info error")
-		return
-	}
-	defer file.Close()
+	files := r.MultipartForm.File["files"]
+	for _, fileHeader := range files {
+		file, err := fileHeader.Open()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer file.Close()
 
-	resFile, err := os.Create(handler.Filename)
-	if err != nil {
-		fmt.Fprintln(w, err)
-		return
+		out, err := os.Create(fileHeader.Filename)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer out.Close()
+
+		_, err = io.Copy(out, file)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
-	defer resFile.Close()
-	io.Copy(resFile, file)
-	fmt.Fprintf(w, "Upload file success\n")
+	fmt.Fprintf(w, `{"code":0,"msg":"Upload file success!"}`)
 }
